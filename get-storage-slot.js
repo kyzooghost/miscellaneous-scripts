18ec39330a840e5ae4511de3c62758e3676e0d55
kyzooghost <73516204+kyzooghost@users.noreply.github.com>
2025-08-14 18:53:17 +1000

    Update get-storage-slot.js

--- get-storage-slot.js
+++ get-storage-slot.js
@@ -1,3 +1,5 @@
+// Run in browser console with Metamask connected
+
 const slot = "0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103";
 const proxyAddress = "0x33bf916373159A8c1b54b025202517BfDbB7863D";
 
@@ -6,4 +8,4 @@ const result = await ethereum.request({
   params: [proxyAddress, slot, "latest"]
 });
 
-console.log(result)
+console.log("0x" + result.slice(-40));
